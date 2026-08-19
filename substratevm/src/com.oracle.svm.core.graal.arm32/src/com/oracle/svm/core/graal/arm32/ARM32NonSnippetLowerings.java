954b28e45dd595e9abc8dec1331e1b0d30bedcee
boxp <tiyotiyouda@gmail.com>
2026-08-19 02:27:14 +0000

    fix: add ARM32 NonSnippetLowerings and SnippetsFeature for LLVM backend
    Add ARM32NonSnippetLowerings and ARM32SnippetsFeature to register
    BytecodeExceptionNode and ThrowBytecodeExceptionNode lowerings for
    the LLVM backend when compiling for ARM32.
    
    Without these, the LLVM backend fails with:
      GraalError: should not reach here: Node implementing Lowerable not handled:
      BytecodeException#NULL_POINTER
    
    This mirrors the RISCV64NonSnippetLowerings and RISCV64SnippetsFeature
    pattern used for RISC-V LLVM backend support.
    
    Refs: BOXP-152

---
+++ substratevm/src/com.oracle.svm.core.graal.arm32/src/com/oracle/svm/core/graal/arm32/ARM32NonSnippetLowerings.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package com.oracle.svm.core.graal.arm32;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+import jdk.graal.compiler.graph.Node;
+import jdk.graal.compiler.options.OptionValues;
+import jdk.graal.compiler.phases.util.Providers;
+
+import com.oracle.svm.core.graal.meta.RuntimeConfiguration;
+import com.oracle.svm.core.graal.snippets.NodeLoweringProvider;
+import com.oracle.svm.core.graal.snippets.NonSnippetLowerings;
+
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+
+/**
+ * ARM32 non-snippet lowerings. Registers BytecodeExceptionNode and ThrowBytecodeExceptionNode
+ * lowerings which are required for the LLVM backend on ARM32.
+ */
+final class ARM32NonSnippetLowerings extends NonSnippetLowerings {
+
+    public static void registerLowerings(RuntimeConfiguration runtimeConfig, Predicate<ResolvedJavaMethod> mustNotAllocatePredicate, OptionValues options,
+                    Providers providers, Map<Class<? extends Node>, NodeLoweringProvider<?>> lowerings, boolean hosted) {
+        new ARM32NonSnippetLowerings(runtimeConfig, mustNotAllocatePredicate, options, providers, lowerings, hosted);
+    }
+
+    private ARM32NonSnippetLowerings(RuntimeConfiguration runtimeConfig, Predicate<ResolvedJavaMethod> mustNotAllocatePredicate, OptionValues options,
+                    Providers providers, Map<Class<? extends Node>, NodeLoweringProvider<?>> lowerings, boolean hosted) {
+        super(runtimeConfig, mustNotAllocatePredicate, options, providers, lowerings, hosted);
+    }
+}
